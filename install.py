772b23a9c63a81638b68beed85e71077c84360af
quellen-sol <marquelle.nesbitt@gmail.com>
2023-01-12 17:54:34 -0800

    Pull from system command

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 import os
 from os import path
+from sys import argv
 
 # key = Name
 # value = Copy Location
@@ -9,6 +10,10 @@ filemap = {
   "starship.toml": "~/.config/",
 }
 
+# Commands
+installCommands = ["install", "i"]
+pullCommands = ["pull", "p", "copy"]
+
 thisFilePath = path.dirname(path.realpath(__file__))
 
 def appendTrailingSlash(path):
@@ -17,7 +22,6 @@ def appendTrailingSlash(path):
   return path
 
 def install():
-  os.system("git pull") # Pull latest dotfiles if needed
   numberFailed = 0
   for filename, copyLocation in filemap.items():
     copyLocation = appendTrailingSlash(os.path.expanduser(copyLocation))
@@ -31,5 +35,36 @@ def install():
     print(f"Failed to copy {numberFailed} files")
     exit(1)
   
+def pullFromSystem():
+  # copy all files in filemap to dotfiles directory
+  numFailed = 0
+  for filename, copyLocation in filemap.items():
+    copyLocation = appendTrailingSlash(os.path.expanduser(copyLocation))
+    result = os.system("cp " + copyLocation + filename + " " + thisFilePath + "/dotfiles/")
+    if (result != 0):
+      numFailed += 1
+      print("Failed to copy " + filename + " to " + copyLocation)
+    else:
+      print("Copied " + filename + " to " + copyLocation)
+  if numFailed > 0:
+    print(f"Failed to copy {numFailed} files")
+    exit(1)
+  
+def main():
 
-install()
+  # If no args, just run `install()`
+  if (len(argv) == 0):
+    install()
+
+  cmd = argv[1].lower()
+
+  if cmd in installCommands:
+    install()
+  elif cmd in pullCommands:
+    pullFromSystem()
+  else:
+    print(f"Invalid command {cmd}")
+    exit(1)
+  
+
+main()
